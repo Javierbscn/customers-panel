d1c4e7df2a0399c0804e2a3cb1a54e68a169c6f1
JavierBoscan <javierboscan16@gmail.com>
2021-02-01 22:24:18 -0400

    consumiendo servicio en configuration.component y metodo para guardar cambios

--- src/app/components/configuration/configuration.component.ts
+++ src/app/components/configuration/configuration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConfigurationService } from 'src/app/services/configuration.service';
+import { Configuration } from 'src/app/models/configuration.model';
 
 @Component({
     selector: 'app-configuration',
@@ -8,13 +11,23 @@ import { Component, OnInit } from '@angular/core';
 export class ConfigurationComponent implements OnInit {
     allowRegister: boolean;
 
-    constructor() { }
+    constructor(
+        private _router: Router,
+        private configurationService: ConfigurationService
+    ) { }
 
     ngOnInit(): void {
         this.allowRegister = false;
+        this.configurationService.getConfiguration().subscribe((configuration: Configuration) => {
+            this.allowRegister = configuration.allowRegister;
+        })
     }
 
-    save(): void {
-        
+    saveChanges(): void {
+        let configuration = { allowRegister: this.allowRegister };
+
+        this.configurationService.setConfiguration(configuration);
+
+        this._router.navigate(['/']);
     }
 }
